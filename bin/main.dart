c3129ad9cca752912226d5f219cc6836310ebcb6
thunder775 <rahulmasih61@gmail.com>
2019-12-10 10:23:51 +0530

    done part 1

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,17 @@
-class Student{
+import 'dart:math';
+
+class Student {
   String name;
   int score;
 
   Student(this.name, this.score);
+  @override
+  String toString() {
+    return 'Name: ${name}, Score: ${score}';
+  }
+
+  @override
+  bool operator ==(o) => o is Student && o.score == score;
 }
 // Challenge  1 - Create 3 Students
 // 1. Ram - 87
@@ -17,7 +26,16 @@ class Student{
 
 // Challenge 4 - Sort the students based on their scores
 
-
 void main() {
-
+  var s1 = Student('Ram', 87);
+  var s2 = Student('Shyam', 80);
+  var s3 = Student('Ghanshyam', 91);
+  var students = [s1, s2, s3];
+  for (var student in students) {
+    print(student);
+  }
+  print(students.reduce((Student a, Student b) => a.score > b.score ? a : b));
+  students.sort((Student a, Student b) =>
+      a.score == b.score ? 0 : (a.score < b.score ? -1 : 1));
+  print(students);
 }
