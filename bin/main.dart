0d07e8847ada3581880985740b9367a2d824c30b
McLarenCollege <hello@mclarencollege.in>
2019-12-10 08:26:42 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,23 @@
+class Student{
+  String name;
+  int score;
+
+  Student(this.name, this.score);
+}
+// Challenge  1 - Create 3 Students
+// 1. Ram - 87
+// 2. Shyam - 80
+// 3. Ghanshyam - 91
+
+// Put these students in a list called 'students'
+
+// Challenge 2 - Print out the student name and score for all students
+
+// Challenge 3 - Find student with Maximum score and print his name and score
+
+// Challenge 4 - Sort the students based on their scores
+
+
+void main() {
+
+}
